11ad8ed9af9e9a8525d3d01948dcc1ede10c831b
DisgracedM8 <52089788+DisgracedM8@users.noreply.github.com>
2023-01-03 03:10:37 -0700

    Resume

--- Java.js
+++ Java.js
@@ -1,32 +1,27 @@
-    $( document ).ready(function() {
-        
-        var taged = $(".NameB");
-  
-        for (var i = 0; i < taged.length; i++) {
-          var tags = taged[i];
-            $(tags).addClass("visible");
+$( document ).ready(function() {
+    
+    var taged = $(".NameB");
+
+    for (var i = 0; i < taged.length; i++) {
+      var tags = taged[i];
+        $(tags).addClass("visible");
+    }
+
+    $(document).on("scroll", function() {
+        var pageTop = $(document).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var tags = $(".NameB");
+
+        for (var i = 0; i < tags.length; i++) {
+          var tag = tags[i];
+          if ($(tag).position().top > pageTop / .5) {
+            $(tag).addClass("visible");
+          } 
+          else {
+            $(tag).removeClass("visible");
+          }
         }
+      });
+});
 
-        $(document).on("scroll", function() {
-            var pageTop = $(document).scrollTop();
-            var pageBottom = pageTop + $(window).height();
-            var tags = $(".NameB");
-
-            for (var i = 0; i < tags.length; i++) {
-              var tag = tags[i];
-              if ($(tag).position().top > pageTop / .5) {
-                $(tag).addClass("visible");
-              } 
-              else {
-                $(tag).removeClass("visible");
-              }
-            }
-            console.log($(taged).position().top);
-            console.log(pageTop);
-            console.log(pageBottom);
-          });
-
-
-
-    });
 
