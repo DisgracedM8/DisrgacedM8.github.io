124eff78c91fdd876ff38e1d36010013f8f91d4e
DisgracedM8 <52089788+DisgracedM8@users.noreply.github.com>
2023-02-11 16:37:42 -0700

    Formatting is all fixed

--- java.js
+++ java.js
@@ -1,6 +1,13 @@
-$( document ).ready(function() {
-  
-  window.onscroll = function() {
+$(document).ready(function () {
+
+  const navbar1 = document.querySelector('.navbar-bgoff');
+  if (window.pageYOffset > 0) {
+    navbar1.classList.add('navbar-bgon');
+  } else {
+    navbar1.classList.remove('navbar-bgon');
+  }
+
+  window.onscroll = function () {
     const navbar = document.querySelector('.navbar-bgoff');
     if (window.pageYOffset > 0) {
       navbar.classList.add('navbar-bgon');
@@ -9,59 +16,60 @@ $( document ).ready(function() {
     }
   };
 
-    const links = document.querySelectorAll('a[href^="#"]');
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if(entry.isIntersecting){
-          entry.target.classList.add('show');
-        } else{
-          entry.target.classList.remove('show');
-        }
-      });
-    });
-  
-    const hiddenElements = document.querySelectorAll('.hidden');
-  
-    hiddenElements.forEach((el) => observer.observe(el));
-  
-    const observer2 = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if(entry.isIntersecting){
-          entry.target.classList.add('show');
-        } else{
-          entry.target.classList.remove('show');
-        }
-      });
-    });
-  
-    const hiddenElements2 = document.querySelectorAll('.hidden2');
-  
-    hiddenElements2.forEach((el) => observer.observe(el));
-    
-      var taged = $(".NameB");
-  
-      for (var i = 0; i < taged.length; i++) {
-        var tags = taged[i];
-          $(tags).addClass("visible");
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
       }
-  
-      $(document).on("scroll", function() {
-          var pageTop = $(document).scrollTop();
-          var tags = $(".NameB");
-  
-          for (var i = 0; i < tags.length; i++) {
-            var tag = tags[i];
-            if ($(tag).position().top > pageTop / .15) {
-              $(tag).addClass("visible");
-            } 
-            else {
-              $(tag).removeClass("visible");
-            }
-          }
-        });
-        
+    });
   });
-  
-  
-  
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+
+  hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+  const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  const hiddenElements2 = document.querySelectorAll('.hidden2');
+
+  hiddenElements2.forEach((el) => observer.observe(el));
+
+
+
+  var taged = $(".NameB");
+
+  for (var i = 0; i < taged.length; i++) {
+    var tags = taged[i];
+    $(tags).addClass("visible");
+  }
+
+  $(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var tags = $(".NameB");
+
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+      if ($(tag).position().top > pageTop / .15) {
+        $(tag).addClass("visible");
+      }
+      else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+
+});
+
+
